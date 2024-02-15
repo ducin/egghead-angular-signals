8e39e41239216e4459d28433f806a3864f7a788a
Tomasz Ducin <tomasz@ducin.dev>
2024-02-15 21:38:07 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -6,25 +6,6 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  template: `
-    <h1>Hello, {{ title }}</h1>
-    <ul>
-    @for (item of lastItem(); track 'id') {
-      <li>{{item.name}}</li>
-    }
-    </ul>
-  `,
-  styleUrl: './app.component.css'
+  template: ` <h1>Angular signals</h1> `,
 })
-export class AppComponent {
-  title = 'signals';
-
-  items = signal([
-    {id: 1, name: 'Andy'},
-    {id: 2, name: 'Bob'},
-    {id: 3, name: 'Charlie'},
-  ])
-
-  lastItem = computed(() => 
-  this.items().slice(-1))
-}
+export class AppComponent {}
