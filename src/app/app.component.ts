5d2d749aac37f116740a610c000d728d1208e8ec
Tomasz Ducin <tomasz@ducin.dev>
2024-03-06 15:56:09 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,28 @@
-import { Component, computed, model, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule],
-  template: `
-    <h1>Angular signals</h1>
-    <input
-      type="text"
-      [value]="nameFilter()"
-      (input)="updateNameFilter($event)"
-    />
-    <input type="text" [(ngModel)]="nameFilter" />
+  imports: [CommonModule, RouterOutlet],
+  template: ` <h1>Angular signals</h1>
+    {{ item() }}
+
+    {{ item()?.toLowerCase() }}
+
     <ul>
-      @for (item of visibleItems(); track 'id') {
+      @for(item of items(); track 'id'){
       <li>{{ item.name }}</li>
       }
-    </ul>
-    {{nameFilter().toLowerCase()}}
-  `,
-  styleUrl: './app.component.css',
+    </ul>`,
 })
 export class AppComponent {
+  item = signal<string | undefined>('hey');
+
   items = signal([
     { id: 1, name: 'Andy' },
     { id: 2, name: 'Bob' },
     { id: 3, name: 'Charlie' },
   ]);
-
-  // nameFilter = signal('')
-  nameFilter = signal<string | undefined>(undefined)
-
-  updateNameFilter($event: Event) {
-    this.nameFilter.set(($event.target as HTMLInputElement)['value']);
-  }
-
-  lastItem = computed(() => this.items().slice(-1));
-
-  visibleItems = computed(() => {
-    // return this.items().filter(item => item.name.includes(this.nameFilter()))
-    const nameFilter = this.nameFilter().toLowerCase();
-    return this.items().filter((item) => {
-      return item.name.toLowerCase().includes(nameFilter);
-    });
-  });
 }
