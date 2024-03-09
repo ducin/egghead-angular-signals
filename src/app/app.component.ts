dcba46b60491ae4cbf6aee136ef634f3f67c455a
Tomasz Ducin <tomasz@ducin.dev>
2024-03-09 12:40:07 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,55 @@
-import { Component, computed, model, signal } from '@angular/core';
+import { Component, Signal, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule],
-  template: `
-    <h1>Angular signals</h1>
-    <input
-      type="text"
-      [value]="nameFilter()"
-      (input)="updateNameFilter($event)"
-    />
-    <input type="text" [(ngModel)]="nameFilter" />
+  imports: [CommonModule, RouterOutlet],
+  template: ` <h1>Angular signals</h1>
+    last: {{ lastItem().name }}
+
+    <button (click)="handleClick()">log items</button>
     <ul>
-      @for (item of visibleItems(); track 'id') {
+      @for(item of visibleItems(); track 'id'){
       <li>{{ item.name }}</li>
       }
     </ul>
-    {{nameFilter().toLowerCase()}}
-  `,
-  styleUrl: './app.component.css',
+    {{ b() }}`,
 })
 export class AppComponent {
+  handleClick() {
+    console.log(this.items());
+  }
+
+  lastItem = computed(() => this.items().slice(-1)[0]);
+
   items = signal([
     { id: 1, name: 'Andy' },
     { id: 2, name: 'Bob' },
     { id: 3, name: 'Charlie' },
   ]);
 
-  // nameFilter = signal('')
-  nameFilter = signal<string | undefined>(undefined)
+  nameFilter = signal('');
 
-  updateNameFilter($event: Event) {
-    this.nameFilter.set(($event.target as HTMLInputElement)['value']);
-  }
-
-  lastItem = computed(() => this.items().slice(-1));
-
-  visibleItems = computed(() => {
+  filteredItems = computed(() => {
     // return this.items().filter(item => item.name.includes(this.nameFilter()))
     const nameFilter = this.nameFilter().toLowerCase();
-    return this.items().filter((item) => {
-      return item.name.toLowerCase().includes(nameFilter);
+    return this.items().filter((item) =>
+      item.name.toLowerCase().includes(nameFilter)
+    );
+  });
+
+  ascOrder = signal(true);
+
+  visibleItems = computed(() => {
+    const order = this.ascOrder() ? 1 : -1;
+    return this.filteredItems().sort((a, b) => {
+      return a.name.localeCompare(b.name) * order;
     });
   });
+
+  a = signal('John');
+  b: Signal<string> = computed(() => this.a() + this.c());
+  c: Signal<string> = computed(() => this.a() + this.b());
 }
