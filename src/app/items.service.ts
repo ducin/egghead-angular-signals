171998f52418d5feb9cfe1887b995d255d12a276
Tomasz Ducin <tomasz@ducin.dev>
2024-03-17 02:30:49 +0100

    update

--- src/app/items.service.ts
+++ src/app/items.service.ts
@@ -1,19 +1,27 @@
-import { Injectable, effect, signal, untracked } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import _ from 'lodash';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ItemsService {
-  #items = signal([
-    { id: 1, name: 'Andy' },
-    { id: 2, name: 'Bob' },
-    { id: 3, name: 'Charlie' },
-  ]);
+  #items = signal(
+    [
+      { id: 1, name: 'Andy' },
+      { id: 2, name: 'Bob' },
+      { id: 3, name: 'Charlie' },
+    ]
+    // { equal: _.isEqual }
+  );
 
   items = this.#items.asReadonly();
 
   clearItems() {
-    this.#items.set([]);
+    this.#items.set([
+      { id: 1, name: 'Andy' },
+      { id: 2, name: 'Bob' },
+      { id: 3, name: 'Charlie' },
+    ]);
   }
 
   append(name: string) {
